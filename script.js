27f4c40bc97a98ccc5927e52f492851ffb152eb5
Vivekkumar Tiwari <158731018+Vivekkumar-Tiwari@users.noreply.github.com>
2026-02-07 23:57:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple smooth scroll (future-proof)
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", e => {
+    if (link.getAttribute("href").startsWith("#")) {
+      e.preventDefault();
+      document.querySelector(link.getAttribute("href"))
+        .scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
